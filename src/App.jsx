307763a7bb9d8a5db84440442e180487da1bc23a
TechSage01 <abdulsalamadebimpe6@gmail.com>
2026-09-04 01:19:11 +0100

    style: update background and card color tokens to match Figma design

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import MainLayout from './components/layout/MainLayout.jsx';
+import RightSidebar from './components/layout/RightSidebar.jsx';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" 
+        element={<MainLayout rightSidebar={<RightSidebar />}>
+          <Home/>
+          </MainLayout>
+          }
+        />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
